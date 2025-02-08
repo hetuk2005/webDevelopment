1d8a5e1eca24807a7cca76eff9a6f963d6ba6849
hetuk2005 <patelhetuk2005@gmail.com>
2025-02-08 21:20:08 +0530

    xyz

---
+++ Tours & Travels Website/Explore More.js
@@ -0,0 +1,35 @@
+ScrollReveal().reveal('.btn-explore',{
+    ...scrollrevealOption,
+    delay:2000,
+})
+ScrollReveal().reveal('.btn-more',{
+    ...scrollrevealOption,
+    delay:2000,
+})
+ScrollReveal().reveal('.card',scrollrevealOption)
+
+ScrollReveal().reveal('.card .image',{
+    ...scrollrevealOption,
+    delay:600,
+})
+ScrollReveal().reveal('.card .content-card h4',{
+    ...scrollrevealOption,
+    delay:1600,
+})
+ScrollReveal().reveal('.next .card .content-card  p',{
+    ...scrollrevealOption,
+    delay:2000,
+})
+
+
+
+ScrollReveal().reveal('.next .card .content-card p',{
+    ...scrollrevealOption,
+    delay:600,
+})
+
+
+ScrollReveal().reveal('form .input',scrollrevealOption)
+ScrollReveal().reveal('row .card',scrollrevealOption)
+
+
