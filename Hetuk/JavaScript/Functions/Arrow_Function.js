cd7a29156027b196cd2f73ec118e55c491cc282d
hetuk2005 <patelhetuk2005@gmail.com>
2025-05-22 10:14:31 +0530

    JavaScript Functions

--- Hetuk/JavaScript/Functions/Arrow_Function.js
+++ Hetuk/JavaScript/Functions/Arrow_Function.js
@@ -1,6 +1,35 @@
 /* Arrow Function */
 
-let stringConcatiation = () => {
-    console.log(2 + 2);
+// let stringConcatiation = () => {
+//     console.log(2 + 2);
+// }
+// stringConcatiation();
+
+// Calculator
+
+let num1 = parseInt(process.argv[2]);
+let operator = (process.argv[3]);
+let num2 = parseInt(process.argv[4]);
+
+let calculator = (num1, operator,num2) => {
+    switch (operator) {
+        case "+":
+            console.log("Your Answer Is: ", num1 + num2);
+            break;
+        case "-":
+            console.log("Your Answer Is: ", num1 - num2);
+            break;
+        case "*":
+            console.log("Your Answer Is: ", num1 * num2);
+            break;
+        case "/":
+            console.log("Your Answer Is: ", num1 / num2);
+            break;
+        case "%":
+            console.log("Your Answer Is: ", num1 % num2);
+            break;
+        default:
+        console.log("You Have Not Given Operator/Number1/Number2");
+    }
 }
-stringConcatiation();
+calculator(num1, operator, num2);
