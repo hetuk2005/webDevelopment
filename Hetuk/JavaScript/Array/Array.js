9f0a40666f92a79ec9beaa9b98036a60e18f3b1e
hetuk2005 <patelhetuk2005@gmail.com>
2025-06-17 16:30:29 +0530

    Javascript

---
+++ Hetuk/JavaScript/Array/Array.js
@@ -0,0 +1,28 @@
+let arr = ['Hetuk', 'Mehfooz', 'Farhan', 'Khushi'];
+
+let arr1 = [{ name: 'Hetuk', age: 18 }, { name: 'Mehfooz', age: 25 }];
+
+let arr2 = [false, true, false];
+
+let arr3 = [1, 2, 3, [4, 5, [6, 7, 8,], [9, 10]]];
+
+let arr4 = [
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3]
+];
+
+// for (let i = 1; i <= array.length; i++){
+//     console.log('i: ', i);
+// }
+
+// array.forEach(element) => {
+//     console.log('element: ', element);
+// }
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// array.push(11, 12); To Add Number At The End
+array.pop(); // To Remove The Number From The End
+
+console.log('array: ', array);
