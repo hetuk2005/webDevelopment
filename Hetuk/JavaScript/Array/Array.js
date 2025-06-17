65f1645973bd360ec258e63957e492621d9bbf72
hetuk2005 <patelhetuk2005@gmail.com>
2025-06-17 16:33:03 +0530

    JavaScript

--- Hetuk/JavaScript/Array/Array.js
+++ Hetuk/JavaScript/Array/Array.js
@@ -12,17 +12,17 @@ let arr4 = [
     [1, 2, 3]
 ];
 
-// for (let i = 1; i <= array.length; i++){
-//     console.log('i: ', i);
-// }
-
-// array.forEach(element) => {
-//     console.log('element: ', element);
-// }
-
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // array.push(11, 12); To Add Number At The End
 array.pop(); // To Remove The Number From The End
 
 console.log('array: ', array);
+
+// for (let i = 1; i <= array.length; i++){
+//     console.log('i: ', i);
+// }
+
+// array.forEach((element) => {
+//     console.log('element: ', element);
+// });
