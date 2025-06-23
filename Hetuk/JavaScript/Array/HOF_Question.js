501c4cfb6dd424431082ed95c1e6698ca04ada5f
hetuk2005 <patelhetuk2005@gmail.com>
2025-06-23 23:00:49 +0530

    JavaScript

--- Hetuk/JavaScript/Array/HOF_Question.js
+++ Hetuk/JavaScript/Array/HOF_Question.js
@@ -1,11 +1,16 @@
 // Map Method
 
-// 1. 
+// 1. Capatizing Each Letter Of An Array
 
 let string = ['hetuk', 'mehfooz', 'farhan', 'khushi'];
 
-// let arr = string.map((element, index) => element);
-// console.log(arr);
+let arr = string.map((element, index) => {
+    return element.charAt(0).toUpperCase() + element.slice(1);
+    //  chartAt(0) - It Takes 1st Character Because Of 0;
+    // to.UpperCase() - Makes The Character Uppercase Which Has Given In The charAt(0)
+    // slice(1) - Gives Remainder Of The String 
+});
+console.log(arr);
 
 // 2. Convert Numbers Into Squares
 
@@ -19,14 +24,14 @@ console.log("Sqaures: ", arr1);
 
 // 3. Product Object Returning Only Product Name
 
-// let product = {
-//     Shoes: 'Nike',
-//     Bike: 'Hero Honda',
-//     Car: 'Rolls Royals'
-// }
+let product = [
+    { Shoes: 'Nike' },
+    {Bike: 'Hero Honda'},
+    { Car: 'Rolls Royals' }
+]
 
-// let arr2 = product.map((element, index) => element);
-// console.log("Product Name: ", arr2);
+let arr2 = product.map((element, index) => Object.values(element));
+console.log("Product Name: ", arr2);
 
 // 4. Return Length
 
@@ -50,3 +55,70 @@ let ans1 = num2.filter((element, index) => {
     return element % 2 == 0;
 })
 console.log("Even Numbers: ", ans1);
+
+// 2. No Loger Than 5 Characters
+
+let ans2 = string.filter((element, index) => {
+    return element.length > 5;
+})
+console.log('Name: ', ans2);
+
+// 3. Students Who Scored Above 75%
+
+let students = [71, 75.5, 75.3, 76, 86, 90];
+
+let ans3 = students.filter((element, index) => {
+    return element > 75;
+})
+console.log('Marks Above 75%: ', ans3);
+
+// 4. Returns Only String Staring With Character A
+
+let string2 = ['hetuk', 'mehfooz', 'ajay', 'arun'];
+
+let ans4 = string2.filter((element, index) => {
+    return element.charAt(0) == 'a';
+})
+console.log('Name Starting With A: ', ans4);
+
+// 5. Filter All Falsy Values
+
+let falsy = ['Hetuk', null, 0, 1, false, 2, 3, true, 999, 100, undefined];
+
+let ans5 = falsy.filter(Boolean);
+console.log('Filtered All Falsy Values: ', ans5);
+
+// Reduce
+
+// 1. Calculate The Total Sum Of An Array
+
+let arr4 = num.reduce((acc, curr) => acc + curr);
+console.log(arr4);
+
+// 2. Combine A String To Make Sentence
+
+let sentence1 = ['Hello'];
+let sentence2 = ['World'];
+
+let sentence = sentence1.concat(sentence2).reduce((acc, curr) => acc + ' ' + curr);
+console.log('Final Sentence: ', sentence);
+
+// 3. Total Price For Object
+
+let obj = [
+    { Apple: 95 },
+    { GreenApple: 100 },
+    { PineApple: 70 }
+];
+
+let price = obj.reduce((acc, curr) => acc + Object.values(curr)[0], 0);
+// Object.values(curr)[0] - Extracts Price, 0 - It Gives Number For Addition
+console.log(price);
+
+// 4. How Many Times It Is Repeated
+
+let name = ['Hetuk', 'Pratham', 'Hetuk', 'Hrishi'];
+
+let repeat = name.reduce((acc, curr) => {
+    return 
+})
