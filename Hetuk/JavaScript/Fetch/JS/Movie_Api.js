922aa459fbec0d091319f201ce8e1ec0e1179623
hetuk2005 <patelhetuk2005@gmail.com>
2025-07-04 16:13:55 +0530

    JavaScript

--- Hetuk/JavaScript/Fetch/JS/Movie_Api.js
+++ Hetuk/JavaScript/Fetch/JS/Movie_Api.js
@@ -1,71 +1,98 @@
 const apiKey = `3d15e923`;
 
 const dataLoad = async () => {
-    
-    let random = [{ name: 'ironman' }, { name: 'superman' }, { name: 'avengers' }, { name: 'thor' }, { name: 'loki' }];
-    
-    const api = `https://www.omdbapi.com/?s=scam&apikey=${apiKey}`;
-    
-    try {
-        const response = await fetch(api);
-        const data = await response.json();
-        appendsFunc(data);
-    } catch (error) {
-        console.log('Error: ', error);
-    }
+  myFunction();
+  let random = [
+    { name: "ironman" },
+    { name: "superman" },
+    { name: "avengers" },
+    { name: "thor" },
+    { name: "loki" },
+    { name: "evil death" },
+    { name: "space" },
+    { name: "earth" },
+    { name: "animal" },
+    {
+      name: "disney",
+    },
+  ];
+
+  let movie = Math.round(Math.random() * random.length);
+
+  let moviename = random[movie].name;
+
+  const api = `https://www.omdbapi.com/?s=${moviename}&apikey=${apiKey}`;
+
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    appendsFunc(data);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
 };
 
 const ApiCalling = async (event) => {
-    if (event.key === 'Enter') {
-        const searchApi = document.querySelector('#searchInput');
-        console.log('Search Api: ', searchApi.value.length);
+  if (event.key === "Enter") {
+    const searchApi = document.querySelector("#searchInput");
+    console.log("Search Api: ", searchApi.value.length);
 
-        if (searchApi.value.length === 0) return;
-        
-        const api=`https://www.omdbapi.com/?s=${searchApi.value}&apikey=${apiKey}`
-        
-        try {
-            const response = await fetch(api);
-            const data = await response.json();
-            console.log('Data: ', data);
-            appendsFunc(data);
-            searchApi.value = '';
-        } catch (error) {
-            console.log('Error: ', error);
-        }
+    if (searchApi.value.length === 0) return;
+
+    const api = `https://www.omdbapi.com/?s=${searchApi.value}&apikey=${apiKey}`;
+
+    try {
+      const response = await fetch(api);
+      const data = await response.json();
+      console.log("Data: ", data);
+      appendsFunc(data);
+      searchApi.value = "";
+    } catch (error) {
+      console.log("Error: ", error);
     }
-    return;
+  }
+  return;
 };
 
 const appendsFunc = (data) => {
-    let mainDiv = document.getElementById('info');
-    mainDiv.innerHTML = '';
-    data.Search.forEach(hello => {
-        
-        const cardDiv = document.createElement('div');
-        const img = document.createElement('img');
+  let mainDiv = document.getElementById("info");
+  mainDiv.innerHTML = "";
+  data.Search.forEach((hello) => {
+    const cardDiv = document.createElement("div");
+    const img = document.createElement("img");
 
-        const nameDiv = document.createElement('div');
-        const title = document.createElement('h3');
+    const nameDiv = document.createElement("div");
+    const title = document.createElement("h3");
 
-        const gridDiv = document.createElement('div');
-        const year = document.createElement('p');
-        const imdb = document.createElement('p');
-        const type = document.createElement('p');
+    const gridDiv = document.createElement("div");
+    const year = document.createElement("p");
+    const imdb = document.createElement("p");
+    const type = document.createElement("p");
 
-        cardDiv.className = 'card_div';
-        nameDiv.className = 'name_div';
-        gridDiv.className = 'grid_div';
+    cardDiv.className = "card_div";
+    nameDiv.className = "name_div";
+    gridDiv.className = "grid_div";
 
-        img.src = hello.Poster;
-        title.innerText = `Title: ${hello.Title}`;
-        year.innerText = `Year: ${hello.Year}`;
-        imdb.innerText = `Imdb ID: ${hello.imdbID}`;
-        type.innerText = `Type: ${hello.Type}`;
+    img.src = hello.Poster;
+    title.innerText = `Title: ${hello.Title}`;
+    year.innerText = `Year: ${hello.Year}`;
+    imdb.innerText = `Imdb ID: ${hello.imdbID}`;
+    type.innerText = `Type: ${hello.Type}`;
 
-        gridDiv.append(year, imdb, type);
-        nameDiv.append(title, gridDiv);
-        cardDiv.append(img, nameDiv);
-        mainDiv.append(cardDiv);
-    });
+    gridDiv.append(year, imdb, type);
+    nameDiv.append(title, gridDiv);
+    cardDiv.append(img, nameDiv);
+    mainDiv.append(cardDiv);
+  });
+};
+
+let myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1500);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
 }
