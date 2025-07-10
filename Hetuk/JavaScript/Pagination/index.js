74d1e13009690c5201d55be0962bec841e4c90da
hetuk2005 <patelhetuk2005@gmail.com>
2025-07-10 14:53:37 +0530

    Code Of The Day

--- Hetuk/JavaScript/Pagination/index.js
+++ Hetuk/JavaScript/Pagination/index.js
@@ -1,77 +1,86 @@
 let pages = 1;
 let flag = null;
 let length_of_pagination;
-let prev = document.querySelector('#prev');
-let next = document.querySelector('#next');
+let totalCount;
+let prev = document.querySelector("#prev");
+let next = document.querySelector("#next");
+let calc;
 
 const appendData = (value) => {
+  const main = document.querySelector(".main");
+  const Page_Data = document.querySelector(".numOfPage");
 
-    const main = document.querySelector('.main');
-    const Page_Data = document.querySelector('.numOfPage');
+  if (flag || !flag) {
+    main.innerHTML = "";
+    Page_Data.innerText = "";
+  }
 
-    if (flag || !flag) {
-        main.innerHTML = '';
-        Page_Data.innerText = '';
-    }
+  value.forEach((el) => {
+    let container = document.createElement("div");
+    let id = document.createElement("p");
+    let text = document.createElement("p");
 
-    value.forEach(el => {
-        let container = document.createElement('div');
-        let id = document.createElement('p');
-        let text = document.createElement('p');
+    container.classList.add("container");
 
-        container.classList.add('container');  // Or We Can Give Class Like This:- container.className = 'conatiner';
+    id.innerText = el.id;
+    id.classList.add("id_data");
+    text.innerText = el.title;
 
-        id.innerText = el.id;
-        id.classList.add('id_data');
-        text.innerText = el.title;
-
-        container.append(id, text);
-        main.append(container);
-    });
-    Page_Data.append(pages);
+    container.append(id, text);
+    main.append(container);
+  });
+  Page_Data.append(pages);
 };
 
 const dataFetch = async () => {
-    let api = `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pages}`;
-    try {
-        let res = await fetch(api);
-        let data = await res.json();
-        console.log(data);
-        length_of_pagination = await data.length;
+  let api = `https://jsonplaceholder.typicode.com/posts?_limit=75&_page=${pages}`;
+  try {
+    let res = await fetch(api);
+    let data = await res.json();
+    length_of_pagination = await data.length;
+    totalCount = +res.headers.get("X-Total-Count"); // <-- header key
+    calc = Math.ceil(totalCount / length_of_pagination);
+    console.log("-> length_of_pagination:", length_of_pagination);
+    console.log("-> calc:", calc);
+    console.log("-> pages:", pages);
 
-        if (pages === 1) {
-            prev.setAttribute('disabled', 'true');
-        }
-        if (pages == length_of_pagination) {
-            next.setAttribute('disabled', 'true');
-        }
-        appendData(data);
-    } catch (error) {
-        console.log('Error: ', error);
+    if (pages === 1) {
+      prev.setAttribute("disabled", "true");
     }
+    if (pages == calc && length_of_pagination == totalCount) {
+      next.setAttribute("disabled", "true");
+    }
+
+    appendData(data);
+  } catch (error) {
+    console.log(" error:", error);
+  }
 };
 
 const prevBtnInvokation = () => {
-    if (pages === 1) {
-        prev.setAttribute('disabled', 'true');
-    } else if (pages === 0) {
-        return;
-    } else if (pages > 1) {
-        next.removeAttribute('disabled');
-        pages--;
-        flag = false;
-        dataFetch();
-    }
+  if (pages === 1) {
+    prev.setAttribute("disabled", "true");
+  } else if (pages === 0) {
+    return;
+  } else if (pages > 1) {
+    next.removeAttribute("disabled");
+    pages--;
+    flag = false;
+    dataFetch();
+  }
 };
 
 const nextBtnInvokation = () => {
-    if (pages == length_of_pagination) {
-        next.setAttribute('disabled', 'true');
-        pages--;
-    } else if (pages >= 1) {
-        prev.removeAttribute('disabled');
-    }
-    flag = true;
-    pages++;
-    dataFetch();
+  if (pages === length_of_pagination || pages === calc) {
+    next.setAttribute("disabled", "true");
+    pages--;
+  } else if (pages >= 1) {
+    prev.removeAttribute("disabled");
+  }
+
+  totalCount = totalCount - length_of_pagination;
+  console.log("-> totalCount:", totalCount);
+  flag = true;
+  pages++;
+  dataFetch();
 };
