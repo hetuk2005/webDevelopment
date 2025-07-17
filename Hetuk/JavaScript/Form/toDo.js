b15dc5e83bfbc30cc24e5f56fd0238a79e87b5df
hetuk2005 <patelhetuk2005@gmail.com>
2025-07-17 16:44:43 +0530

    JavaScript

--- Hetuk/JavaScript/Form/toDo.js
+++ Hetuk/JavaScript/Form/toDo.js
@@ -1,43 +1,85 @@
-let arrStorage = JSON.parse(localStorage.getItem("todos")) || [];
+// let text = JSON.parse(localStorage.getItem("todos")) || [];
 
-const myTodos = () => {
-  const todoVal = document.querySelector("#todos").value;
-  if (todoVal.length == 0) {
-    alert("please fill the text...");
-    return;
-  }
+// const Add = () => {
+//   const todoInput = document.querySelector("#input").value;
+//   if (todoInput.length == 0) {
+//     alert("please fill the text...");
+//     return;
+//   }
 
-  let data = {
-    id: Math.random().toString(36).substring(2, 15),
-    todoText: todoVal,
-    isCompleted: false,
-    isEdits: false,
+//   let data = {
+//     id: Math.random().toString(36).substring(2, 15),
+//     todoText: todoInput,
+//     isCompleted: false,
+//     isEdits: false,
+//   };
+
+//   text.unshift(data);
+//   localStorage.setItem("todos", JSON.stringify(todos));
+//   appendData();
+//   alert("Your Data Has Been Added In To LS");
+//   document.querySelector("#input").value = "";
+// };
+
+// const appendData = () => {
+//   const dataInfo = document.querySelector("#text");
+
+//   dataInfo.innerHTML = "";
+
+//   if (text.length == 0) {
+//     let notify = document.createElement("h1");
+//     notify.innerText = "You Don't Have Data...";
+//     dataInfo.append(notify);
+//   }
+//   text &&
+//     text.forEach((el, i) => {
+//       let id = document.createElement("p");
+//       let div = document.createElement("div");
+
+//       div.className = "dataRow";
+
+//       id.innerText = el.id;
+//     });
+// };
+
+let dataBase = JSON.parse(localStorage.getItem("formData")) || [];
+
+function handleForm(e) {
+  e.preventDefault();
+  let todoName = document.querySelector("#taskInput").value;
+
+  let personData = {
+    id: Date.now(),
+    textTodo: todoName,
+    isEdit: false,
+    isComplete: false,
   };
 
-  arrStorage.unshift(data);
-  localStorage.setItem("todos", JSON.stringify(arrStorage));
-  appendData();
-  alert("Your Data Has Been Added In To LS");
-  document.querySelector("#todos").value = "";
-};
+  dataBase.unshift(personData);
+  localStorage.setItem("formData", JSON.stringify(dataBase));
+  renderTodoList();
+  document.querySelector("#taskInput").value = "";
+}
+const renderTodoList = () => {
+  let todoTable = document.getElementById("todoTable");
 
-const appendData = () => {
-  const dataInfo = document.querySelector("#dataInfo");
+  todoTable.innerHTML = "";
+  dataBase?.map((el) => {
+    let div = document.createElement("div");
+    let text = document.createElement("h4");
+    let edits_btn = document.createElement("button");
+    let delete_btn = document.createElement("button");
+    let id = document.createElement("p");
+    let checkBox = document.createElement("input");
 
-  dataInfo.innerHTML = "";
+    checkBox.type = "checkBox";
+    text.innerText = el.textTodo;
+    id.innerText = el.id;
 
-  if (arrStorage.length == 0) {
-    let notify = document.createElement("h1");
-    notify.innerText = "You Don't Have Data...";
-    dataInfo.append(notify);
-  }
-  arrStorage &&
-    arrStorage.forEach((el, i) => {
-      let div = document.createElement("div");
-      let id = document.querySelector("p");
+    edits_btn.innerText = "edits";
+    delete_btn.innerText = "delete";
 
-      div.className = "dataRow";
-
-      id.innerText = el.id;
-    });
+    div.append(checkBox, id, text, edits_btn, delete_btn);
+    todoTable.append(div);
+  });
 };
