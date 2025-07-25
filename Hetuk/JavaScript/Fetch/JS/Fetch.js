3d4983a655e645a40bfdb65a7460dad00c3ad0cc
hetuk2005 <patelhetuk2005@gmail.com>
2025-07-25 23:21:37 +0530

    Fake Store Api

--- Hetuk/JavaScript/Fetch/JS/Fetch.js
+++ Hetuk/JavaScript/Fetch/JS/Fetch.js
@@ -35,39 +35,29 @@ const appendsFunc = (data) => {
     let rating = document.createElement("div");
     let rate = document.createElement("p");
     let count = document.createElement("p");
+    let cart_btn = document.createElement("img");
 
     cardDiv.className = "card_div";
-
     rating.className = "rating";
-
     price.className = "price";
-
     description.className = "description";
-
     category.className = "category";
-
     rate.className = "rate";
-
     count.className = "count";
+    cart_btn.className = "cart_btn";
+    img.className = "div_image";
 
+    cart_btn.src = "./Cart.svg";
     img.src = element.image;
-
     title.innerText = element.title;
-
     price.innerHTML = `<b><u>Price</u>: ${element.price}</b>`;
-
     description.innerHTML = `<b><u>Description</u>: ${element.description}</b>`;
-
     category.innerHTML = `<b><u>Category</u>: ${element.category}</b>`;
-
     rate.innerHTML = `<b><u>Rate</u>: ${element.rating.rate}</b>`;
-
     count.innerHTML = `<b><u>Count</u>: ${element.rating.count}</b>`;
 
     rating.append(rate, count);
-
-    cardDiv.append(img, title, price, description, category, rating);
-
+    cardDiv.append(img, title, price, description, category, rating, cart_btn);
     dataShow.append(cardDiv);
   });
 };
