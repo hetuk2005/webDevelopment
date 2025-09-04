6c6fa3e7154cc70abc43db13c58cf11d7ea6a5a8
hetuk2005 <patelhetuk2005@gmail.com>
2025-09-04 20:03:09 +0530

    xyz

--- Hetuk/Install Old React/react-intro-1/src/App.js
+++ Hetuk/Install Old React/react-intro-1/src/App.js
@@ -15,6 +15,7 @@ function App() {
 
   return (
     <>
+      <h4>Hello</h4>
       <h1 id="counter">Counter {count}</h1>
       <button onClick={handleIncre}>+</button>
       <button onClick={handleDecre}>-</button>
