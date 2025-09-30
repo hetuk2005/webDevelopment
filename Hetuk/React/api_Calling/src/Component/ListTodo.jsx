fd072e980e8fcb114b7e363acb9671df7e10e053
hetuk2005 <patelhetuk2005@gmail.com>
2025-09-30 17:23:08 +0530

    code

--- Hetuk/React/api_Calling/src/Component/ListTodo.jsx
+++ Hetuk/React/api_Calling/src/Component/ListTodo.jsx
@@ -12,17 +12,60 @@ const getData = async (url) => {
 
 export const ListTodo = () => {
     const [apiData, setApiData] = useState(null);
+    const [isLoading, setisLoading] = useState(true);
+    const [isError, setisError] = useState(false);
     console.log("Api Data: ", apiData);
     
     let api = `https://api-database-1.onrender.com/ToDo`
 
-    useEffect(() => {
-        setApiData(getData(api))
-    }, []);
+    const handleEdit = async (id) => {
+
+        try {
+            let updateEdit = {
+            isEdit: true
+            }
+            let res = await axios.patch(`${api}/${id}`, updateEdit);
+            console.log("Res: ", res);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const handleDel = async (id) => {
+        await axios.delete(`${api}/${id}`)
+    }
+
+        useEffect(() => {
+            const fetchData = async () => {
+                try {
+                    let res1 = await getData(api);
+                    setApiData(res1);
+                } catch (error) {
+                    console.log(error);
+                setisError(true)
+                }
+                finally {
+                    setisLoading(false)
+                }
+            }
+            fetchData();
+        }, []);
+    
+if(isLoading) return <h1>Loading...</h1>
+if(isError) return <h1>404 Error</h1>
 
     return (
         <>
             <h1>List Todo</h1>
+            {
+                apiData?.map((el)=>(
+                    <div className="todoList" key={el.id}>
+                        <input type="checkbox" />
+                        <h1>{el.todo}</h1>
+                        <button onClick={()=>handleEdit(el.id)} className="btn-edit">Edit</button>
+                        <button onClick={()=>handleDel(el.id)} className="btn-del">Delete</button>
+                    </div>
+                ))
+            }
         </>
     )
-}
+    }
