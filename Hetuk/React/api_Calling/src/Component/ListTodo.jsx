f6a8e4de39e321f4782f685300d8e74f9d0b671f
hetuk2005 <patelhetuk2005@gmail.com>
2025-10-10 16:29:51 +0530

    Code

--- Hetuk/React/api_Calling/src/Component/ListTodo.jsx
+++ Hetuk/React/api_Calling/src/Component/ListTodo.jsx
@@ -55,7 +55,6 @@ if(isError) return <h1>404 Error</h1>
 
     return (
         <>
-        <h1>Hello Everyone</h1>
             <h1>List Todo</h1>
             {
                 apiData?.map((el)=>(
@@ -69,4 +68,4 @@ if(isError) return <h1>404 Error</h1>
             }
         </>
     )
-    }
+}
