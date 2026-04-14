d54eb03f0bd213189de1d7ecde32da39e518b410
hetuk2005 <patelhetuk2005@gmail.com>
2026-04-14 20:23:41 +0530

    Code

--- Backend/Day_3/index.js
+++ Backend/Day_3/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.write("Hello Server...");
   res.write("Hello Server...2");
-  res.end("\nExit");
+  res.end("\git add -AnExit");
 });
 
 server.listen(8000, () => {
